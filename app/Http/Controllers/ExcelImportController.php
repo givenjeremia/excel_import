764eea5acfc39583acde402ce4def4d01a3d5497
Given Jeremia <jeremiagiven@gmail.com>
2023-05-19 18:57:20 +0700

    update

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Shuchkin\SimpleXLSX;
 use Illuminate\Http\Request;
 use App\Imports\YourImportClass;
 use Maatwebsite\Excel\Facades\Excel;
@@ -14,12 +15,12 @@ class ExcelImportController extends Controller
 
     $tamp = [];
     $data = Excel::toArray(new YourImportClass(), request()->file('excel_file'));
-    foreach ($data as $key => $row) {
-        foreach ($row as $key2 => $item) {
-            # code...
-            if($key2 > 4){
+    // dd(request()->file('excel_file'));
+    if ( $xlsx = SimpleXLSX::parse(request()->file('excel_file')) ) {
+        foreach ($xlsx->rows() as $key => $item) {
+            if($key> 5){
                 $arr = [
-                    'nama_produk'	=> $item['list_produk'],
+                    'nama_produk'	=> $item[0],
                     'satuan' => $item[1],
                     'warna' => $item[2],
                     'bentuk' =>$item[3],
@@ -33,8 +34,36 @@ class ExcelImportController extends Controller
 
                 array_push($tamp,$arr);
             }
+            # code...
         }
+        // dd($xlsx->rows());
+    } else {
+        echo SimpleXLSX::parseError();
     }
+    
+
+
+    // foreach ($data as $key => $row) {
+    //     foreach ($row as $key2 => $item) {
+    //         # code...
+    //         if($key2 > 4){
+    //             $arr = [
+    //                 'nama_produk'	=> $item['list_produk'],
+    //                 'satuan' => $item[1],
+    //                 'warna' => $item[2],
+    //                 'bentuk' =>$item[3],
+    //                 'ukuran_dimensi' => $item[4],
+    //                 'kategori'=> $item[5],
+    //                 'jenis_inventory'=> $item[6],
+    //                 'harga_beli' => $item[7],
+    //                 'harga_jual' => $item[8],
+    //                 'deskripsi' => $item[9]
+    //             ];
+
+    //             array_push($tamp,$arr);
+    //         }
+    //     }
+    // }
     // dd($tamp);
     // return redirect()->back()->with('success', 'Excel file imported successfully.');
 
